49f53428476c79555dc7b17c91f4c782ec40e47c
Alexander <agagarinov@axbit.ru>
2022-10-29 01:37:16 +0300

    Реализована навигация многомодульного приложения

--- app/src/main/java/ru/alexannu/modulestest/MainActivity.kt
+++ app/src/main/java/ru/alexannu/modulestest/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.alexannu.modulestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.alexannu.modules.features.profile.ProfileFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
 }
